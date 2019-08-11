28033d1d050e19d692986ca0b4f6ece56351727e
Srimadden <36351818+Srimadden@users.noreply.github.com>
2019-08-11 10:54:42 +0530

    Update check whether a character is alphabet or not.py

--- check whether a character is alphabet or not.py
+++ check whether a character is alphabet or not.py
@@ -1,5 +1,6 @@
-ch = input("Enter a character: ")
+ch="a"
 if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
-    print(ch, "Alphabet")
+    print("Alphabet")
 else:
-    print(ch, "No")
+    print("No")
+
